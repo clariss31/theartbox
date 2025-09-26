ae537f92d450f0e35f0fd163c880792940997059
Clarisse Ferand <clarisse.ferand@gmail.com>
2025-09-26 11:32:26 +0200

    Etapes 3 et 4 mise à jour de la page d'accueil et page oeuvre

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
-        <?php include('header.php');
-        include('oeuvres.php'); ?>
-        <div id="liste-oeuvres">
-            <?php foreach ($oeuvres as $oeuvre) { ?>
-                <article class="oeuvre">
-                <a href= "oeuvre.php?id=<?php echo $oeuvre['id']; ?>">
-                    <img src="<?php echo $oeuvre['image']; ?>" alt="<?php echo $oeuvre['titre']; ?>">
-                    <h2><?php echo $oeuvre['titre']; ?></h2>
-                    <p class="description"><?php echo $oeuvre['artiste']; ?></p>
-                </a>
-                </article>
-            <?php } ?>
-        </div>
-    <?php include('footer.php'); ?>
+<?php
+    require 'header.php';
+    require 'bdd.php';
+    $bdd = connexion();
+    $oeuvres = $bdd->query('SELECT * FROM oeuvres');
+?>
+<div id="liste-oeuvres">
+    <?php foreach($oeuvres as $oeuvre): ?>
+        <article class="oeuvre">
+            <a href="oeuvre.php?id=<?= $oeuvre['id'] ?>">
+                <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
+                <h2><?= $oeuvre['titre'] ?></h2>
+                <p class="description"><?= $oeuvre['artiste'] ?></p>
+            </a>
+        </article>
+    <?php endforeach; ?>
+</div>
+<?php require 'footer.php'; ?>
--- oeuvre.php
+++ oeuvre.php
@@ -1,27 +1,35 @@
 <?php
     require 'header.php';
-    require 'oeuvres.php';
+    require 'bdd.php';
+    $bdd = connexion();
+    $oeuvres = $bdd->query('SELECT * FROM oeuvres');
 
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
-    if(empty($_GET['id'])) {
+    if(!isset($_GET['id']) || empty($_GET['id']) || !is_numeric($_GET['id'])) {
         header('Location: index.php');
+        exit;
     }
 
-    $oeuvre = null;
+// On récupère l'oeuvre
+$retrieveOeuvreStatement = $bdd->prepare('SELECT * FROM oeuvres WHERE id = :id');
+$retrieveOeuvreStatement->execute([
+    'id' => (int)$_GET['id'],
+]);
+$oeuvre = $retrieveOeuvreStatement->fetch(PDO::FETCH_ASSOC);
 
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }
+if (!$oeuvre) {
+    header('Location: index.php');
+    exit;
+}
 
-    // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
-        header('Location: index.php');
-    }
+$oeuvre = [
+    'id' => $oeuvre['id'],
+    'titre' => $oeuvre['titre'],
+    'description' => $oeuvre['description'],
+    'artiste' => $oeuvre['artiste'],
+    'image' => $oeuvre['image'],
+];
+
 ?>
 
 <article id="detail-oeuvre">
